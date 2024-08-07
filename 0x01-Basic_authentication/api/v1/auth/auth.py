69e5f1ab1fba8f994b02827021713f7f8a696e24
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-08-07 05:23:15 +0300

    doc sting

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 """
-Class Module
+Auth Module
 """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
+    """ Auth Class """
+
+    def __init__(self):
+        """
+            Constructor
+
+            Args:
+                path: path to authenticate
+                excluded_paths: list of excluded path to authenticate
+        """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Returns False for now. Path and excluded_paths will be used later.
