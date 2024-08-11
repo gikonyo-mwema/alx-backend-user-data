65b8530863eaafab068a5cef8597037c05d14456
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-08-11 20:22:46 +0300

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class that inherits from Auth.
+
+    This class is currently empty and can be extended in the future
+    to implement specific authentication methods.
+    """
+    pass  # This class currently does not add any new functionality
